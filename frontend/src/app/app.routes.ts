35206a83166b0c6eba88a91e43bdf9b384b3edcb
Lt1201 <ltennant1201@gmail.com>
2025-03-14 03:46:42 -0400

    implement topics page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TopicsComponent } from './topics/topics.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'topics', pathMatch: 'full' },
+    { path: 'topics', component: TopicsComponent }
+];
---
+++ frontend/src/app/topics/topics.component.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCardModule } from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+
+interface Topic {
+  name: string;
+  description: string;
+  id: number;
+}
+
+@Component({
+  selector: 'app-topics',
+  imports: [MatListModule, MatTooltipModule, MatCardModule, MatInputModule, MatSelectModule, MatFormFieldModule, MatButtonModule, FormsModule],
+  templateUrl: './topics.component.html',
+  styleUrl: './topics.component.scss'
+})
+export class TopicsComponent {
+  topics: Topic[] = [];
+  newTopicName = "";
+  newTopicDescription = "";
+
+  constructor(private http: HttpClient) {
+    http.get<Topic[]>('/api/topic').subscribe((topics) => {
+      console.log(topics);
+      this.topics = topics;
+    });
+  }
+
+  createTopic() {
+    console.log("Creating topic", this.newTopicName, this.newTopicDescription);
+    this.http.post<Topic>('/api/topic', {
+      name: this.newTopicName,
+      description: this.newTopicDescription
+    }).subscribe((topic) => {
+      this.topics.push(topic);
+    });
+  }
+}
